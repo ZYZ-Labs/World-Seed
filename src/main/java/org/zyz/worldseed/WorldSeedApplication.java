d3e4f7c565cea8ef6d95635cc4b5ceb5615edd25
SilverIceKey <z516798599@gmail.com>
2025-02-05 15:56:12 +0800

    第一次提交

---
+++ src/main/java/org/zyz/worldseed/WorldSeedApplication.java
@@ -0,0 +1,13 @@
+package org.zyz.worldseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldSeedApplication.class, args);
+    }
+
+}
