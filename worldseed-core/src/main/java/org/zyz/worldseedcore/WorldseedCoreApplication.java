d86b4407b351a07673454efe1c8885111b1e92a7
SilverIceKey <z516798599@gmail.com>
2025-03-21 17:17:45 +0800

    feat(main): 删除没用的代码

---
+++ worldseed-core/src/main/java/org/zyz/worldseedcore/WorldseedCoreApplication.java
@@ -0,0 +1,13 @@
+package org.zyz.worldseedcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldseedCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldseedCoreApplication.class, args);
+	}
+
+}
